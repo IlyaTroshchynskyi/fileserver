dc04155c85720ad6c4cc48ece4a3df61aafea2ac
Ilya <itroshchinskiy@rambler.ua>
2021-05-11 23:33:37 +0300

    added new function define_symbols_for_name

--- utils.py
+++ utils.py
@@ -3,17 +3,20 @@ import random
 import datetime
 
 
-def generate_file_name(length_name, extension, letters=False, digit=False):
+def define_symbols_for_name(letter=False, digit=False):
     possible_symbols = ''
-    if letters and digit:
+    if letter and digit:
         possible_symbols = ascii_letters + digits
-    elif letters:
+    elif letter:
         possible_symbols = ascii_letters
     elif digit:
         possible_symbols = digits
+    return possible_symbols
 
+
+def generate_file_name(length_name, extension, letter, digit):
+    possible_symbols = define_symbols_for_name(letter, digit)
     return ''.join(random.choices(list(possible_symbols), k=length_name)) + extension
-
 
 
 def conversion_date_time(data):
