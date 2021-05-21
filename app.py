fa4a431e51de520955d2f82351606e0c629a02f4
Ilya <itroshchinskiy@rambler.ua>
2021-05-21 19:32:38 +0300

    added func del files

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+import file_service
+from config import Configuration
 
 
 app = Flask(__name__)
+app.config.from_object(Configuration)
 
 
 @app.route('/')
 def index():
     files = [file for file in os.listdir() if os.path.isfile(file)]
     return render_template('index.html', files=files)
+
+
+@app.route('/delete/path_file')
+def delete_file(path_file):
+
+    file_service.delete_file(path_file)
+
+    return render_template('index.html')
+
+
 
 
 if __name__ == '__main__':
