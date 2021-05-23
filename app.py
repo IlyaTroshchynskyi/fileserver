c117f41c75b68d690dd526566b7837c31b327a46
Ilya <itroshchinskiy@rambler.ua>
2021-05-23 15:05:21 +0300

    created define path func, refactoring

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, url_for, request, flash, send_from_directory
+from flask import Flask, render_template, redirect, url_for, request, flash, \
+    send_from_directory
 import file_service
 from config import Configuration
 from werkzeug.utils import secure_filename
@@ -9,29 +10,28 @@ app = Flask(__name__)
 app.config.from_object(Configuration)
 
 
+def define_path_to_file(file_name):
+    return os.path.join(os.getcwd(), 'tests_file', file_name)
+
+
 @app.route('/')
 def index():
     files = [file for file in os.listdir(os.path.join(os.getcwd(), 'tests_file'))]
     return render_template('index.html', files=files)
 
 
-@app.route('/delete/<name_file>', methods=['POST'])
-def delete_file(name_file):
-    name_file = os.path.join(os.getcwd(), 'tests_file', name_file)
-    file_service.delete_file(name_file)
+@app.route('/delete/<file_name>', methods=['POST'])
+def delete_file(file_name):
+    path_to_file = define_path_to_file(file_name)
+    file_service.delete_file(path_to_file)
+    return redirect(url_for('index'))
 
-    return redirect (url_for('index'))
 
-
-@app.route('/read_file/<name_file>')
-def read_file(name_file):
-    print(name_file)
-    path = os.path.join(os.getcwd(), 'tests_file', name_file)
-    print(path)
-    extension = os.path.splitext(name_file)[-1]
-    print(extension)
+@app.route('/read_file/<file_name>')
+def read_file(file_name):
+    path = define_path_to_file(file_name)
+    extension = os.path.splitext(file_name)[-1]
     if extension == '.txt':
-        print('read txt')
         content = file_service.read_file(path_to_file=path)
     elif extension == '.json':
         content = file_service.read_json_file(path_to_file=path)
@@ -41,23 +41,22 @@ def read_file(name_file):
         content = file_service.read_csv_file(path_to_file=path)
     else:
         content = ''
-    print(content)
     return render_template('output_file.html', content=content, extension=extension)
 
 
-@app.route('/update/<name_file>', methods=['GET', 'POST'])
-def update_file(name_file):
+@app.route('/update/<file_name>', methods=['GET', 'POST'])
+def update_file(file_name):
+    path = define_path_to_file(file_name)
     if request.method == 'POST':
         content = request.form.get('content', '')
-        with open(os.path.join(os.getcwd(), 'tests_file', name_file), 'w') as file:
+
+        with open(path, 'w') as file:
             file.write(content)
 
-        flash(f'File {name_file} was successfully updated', category='success')
-
+        flash(f'File {file_name} was successfully updated', category='success')
         return redirect(url_for('index'))
 
-    content = file_service.read_file(os.path.join(os.getcwd(), 'tests_file', name_file))
-
+    content = file_service.read_file(path)
     return render_template('update_file.html', content=content)
 
 
@@ -68,27 +67,23 @@ def allowed_file(filename):
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 @app.route('/upload_file', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
-            print(request.url)
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file. Please choose the file')
+            flash('No selected file. Please choose the file', 'danger')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash(f'File {file.filename} was successfully uploaded', category='success')
             return redirect(url_for('index'))
 
     return render_template('upload_file.html')
@@ -96,9 +91,25 @@ def upload_file():
 
 @app.route('/parse-rules', methods=['GET', 'POST'])
 def pars_rules():
-    content = file_service.parse_rules('InputOutputValidation_v2.xlsx')
+    content = [['Rule ID', 'Rule description', 'Result formula', 'Result amount', 'Status', 'LHS', 'RHS']]
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file. Please choose the file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            path = define_path_to_file(filename)
+            content = file_service.parse_rules(path)
 
     return render_template('parse_rules.html', content=content)
+
+
+@app.route('/get-meta-data/<file_name>')
+def get_meta_data(file_name):
+    path = define_path_to_file(file_name)
+    data = file_service.get_metadata_file(path)
+    return data
 
 
 if __name__ == '__main__':
